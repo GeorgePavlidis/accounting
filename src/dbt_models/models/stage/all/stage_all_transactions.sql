74ba32309089d3271e887ea9647f860498d39b79
georgepavlidis <george030796@gmail.com>
2024-10-28 14:08:33 +0100

    save everything

--- src/dbt_models/models/stage/all/stage_all_transactions.sql
+++ src/dbt_models/models/stage/all/stage_all_transactions.sql
@@ -3,8 +3,14 @@
 
 WITH ing AS (
     SELECT
-         cast(NULL as STRING) AS Prodict,
-        * EXCEPT(Counterparty, Code, Debit_credit, Balance, Account)
+        Date,
+        Bank,
+        Amount,
+        Description,
+        Type,
+        cast(NULL as STRING) AS Product,
+        Notifications,
+        Tag
     FROM {{ ref('stage_ing') }}
     WHERE
         NOT (REGEXP_CONTAINS(`Description`, r'Revolut') OR REGEXP_CONTAINS(Counterparty, r'REVO'))
@@ -12,18 +18,37 @@ WITH ing AS (
 
 revolut AS (
     SELECT
-        cast(`Started Date` AS TIMESTAMP) AS Date,
+        `Started Date`  AS Date,
+        Bank,
+        Amount,
+        Description,
+        Type,
+        Product,
         cast(NULL as STRING) AS Notifications,
-        cast(NULL as STRING) AS Tag,
-        * EXCEPT(`Started Date`, `Completed Date`, Fee, Currency, State, rownum, Balance)
+        CAST('' as STRING) AS Tag
     FROM {{ ref('stage_revolut') }}
     WHERE
         NOT (Type = 'TOPUP' AND (Description = 'OBA topup from Hr G Pavlidis' OR Description = 'IDEAL Top-Up'))
       AND Currency = 'EUR'
-)
+),
+original AS (
+    SELECT *
+    FROM ing
+    UNION ALL
+    SELECT *
+    FROM revolut
+    order by Date desc
+),
 
+orginal_with_tags AS (
+    SELECT * except(Tag),
+        CASE
+            WHEN REGEXP_CONTAINS(Description, r"(Dirk|Albert Heijn|Jumbo)") THEN
+                IF(Tag IS NOT NULL AND Tag != '', CONCAT(Tag, ', supermarket'), 'supermarket')
+            ELSE
+                Tag
+        END AS Tag
+    FROM original
+)
 SELECT *
-FROM ing
-UNION ALL
-SELECT *
-FROM revolut
+FROM orginal_with_tags
--- src/dbt_models/models/stage/all/stage_main_table.sql
+++ src/dbt_models/models/stage/all/stage_main_table.sql
@@ -1,205 +1,60 @@
--- Use the `ref` function to select from other models
 {{ config(materialized='table') }}
 
 WITH original AS (
-        SELECT *,
-             DATE_TRUNC(
-                DATE_SUB(DATE(Date), INTERVAL 24 DAY), 
-                MONTH
-            ) AS custom_month_start,
-        FROM {{ ref('stage_all_transactions') }}
-{#                  WHERE#}
-{#    Date >= TIMESTAMP ('2024-09-25') AND Date#}
-{#   < TIMESTAMP ('2024-10-24')#}
+    SELECT *,
+        DATE_TRUNC(DATE_SUB(DATE(Date), INTERVAL 24 DAY), MONTH) AS custom_month_start
+    FROM {{ ref('stage_all_transactions') }}
 ),
-Income AS (
+
+-- Filter positive and specific records only once here to avoid repetition
+filtered_transactions AS (
     SELECT
         custom_month_start,
-        SUM(Amount) AS Income
-    FROM
-        original
-    WHERE Amount > 0 and not regexp_contains(Notifications, 'Oranje spaarrekening')
-    GROUP BY custom_month_start
+        Amount,
+        Notifications,
+        Description,
+        CASE WHEN Amount > 0 AND NOT REGEXP_CONTAINS(Notifications, 'Oranje spaarrekening') THEN Amount ELSE 0 END AS income_amount,
+        CASE WHEN REGEXP_CONTAINS(Notifications, 'Oranje spaarrekening') THEN -Amount ELSE 0 END AS savings_amount,
+        CASE WHEN REGEXP_CONTAINS(Description, 'Hr I Trantas') THEN -Amount ELSE 0 END AS roomate_amount,
+        CASE WHEN REGEXP_CONTAINS(Description, 'CBRE DRES Custodian I BV inzCBRE DRES II Actys R') THEN Amount ELSE 0 END AS rent_amount,
+        CASE WHEN REGEXP_CONTAINS(Description, 'Vattenfall Klantenservice N.V.') THEN Amount ELSE 0 END AS electricity_and_gas_amount,
+        CASE WHEN REGEXP_CONTAINS(Description, 'Waternet') THEN Amount ELSE 0 END AS water_amount,
+        CASE WHEN REGEXP_CONTAINS(Notifications, 'Odido Internet + TV') THEN Amount ELSE 0 END AS internet_amount,
+        CASE WHEN Description='Gemeente Amsterdam Belastingen' THEN Amount ELSE 0 END AS Municipality_Tax_amount,
+        CASE WHEN REGEXP_CONTAINS(Notifications, r'(?i)Unive') THEN Amount ELSE 0 END AS insurance_amount,
+        CASE WHEN REGEXP_CONTAINS(Notifications, 'ODIDO NETHERLANDS B.V.') and REGEXP_CONTAINS(Description, 'Mob') THEN Amount ELSE 0 END AS phone_amount,
+        CASE WHEN REGEXP_CONTAINS(Description, r'(Dirk|Albert Heijn|Jumbo)') THEN Amount ELSE 0 END as supermarket_amount
+
+    FROM original
 ),
-Savings AS (
+
+-- Aggregate values by custom_month_start
+aggregated AS (
     SELECT
         custom_month_start,
-        SUM(Amount)*-1 AS Savings
-    FROM
-        original
-    WHERE regexp_contains(Notifications, 'Oranje spaarrekening')
-    GROUP BY custom_month_start
-),
-Roomate AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount)*-1 AS Roomate
-    FROM
-        original
-    WHERE regexp_contains(Description, 'Hr I Trantas')
-    GROUP BY custom_month_start
-),
-Rent AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Rent
-    FROM
-        original
-    WHERE regexp_contains(Description, 'CBRE DRES Custodian I BV inzCBRE DRES II Actys R')
-    GROUP BY custom_month_start
-),
-electricity_and_gas AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Electricity_and_Gas
-    FROM
-        original
-    WHERE regexp_contains(Description, 'Vattenfall Klantenservice N.V.')
-    GROUP BY custom_month_start
-),
-water AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Water
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-internet AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Internet
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-municipality_tax AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Municipality_Tax
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-home_stuff AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Home_Stuff
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-coffee AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Coffee
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-insurance AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Insurance
-    FROM
-        original
-    WHERE regexp_contains(Notifications, r'(?i)Unive')
-    GROUP BY custom_month_start
-),
-phone AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Phone
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-car AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Car
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-monthly_spending AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Monthly_Spending
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-clothes AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Clothes
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-new_stuff AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS New_Stuff
-    FROM
-        original
-    WHERE Amount > 0
-    GROUP BY custom_month_start
-),
-other AS (
-    SELECT
-        custom_month_start,
-        SUM(Amount) AS Other
-    FROM
-        original
-    WHERE Amount > 0
+        SUM(income_amount) AS Income,
+        SUM(savings_amount) AS Savings,
+        SUM(roomate_amount) AS Roomate,
+        SUM(rent_amount) AS Rent,
+        SUM(electricity_and_gas_amount) AS Electricity_and_Gas,
+        SUM(water_amount) AS Water,
+        SUM(internet_amount) AS Internet,
+        SUM(Municipality_Tax_amount) AS Municipality_Tax,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'Home Supplies') THEN Amount ELSE 0 END) AS Home_Stuff,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'Coffee Purchase') THEN Amount ELSE 0 END) AS Coffee,
+        SUM(insurance_amount) AS Insurance,
+        SUM(phone_amount) AS Phone,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'Car Expenses') THEN Amount ELSE 0 END) AS Car,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'Clothing') THEN Amount ELSE 0 END) AS Clothes,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'New Purchases') THEN Amount ELSE 0 END) AS New_Stuff,
+        SUM(CASE WHEN Amount > 0 AND REGEXP_CONTAINS(Description, 'Miscellaneous') THEN Amount ELSE 0 END) AS Other,
+        SUM(supermarket_amount) AS sumermarket,
+        SUM(Amount) as Balance
+
+    FROM filtered_transactions
     GROUP BY custom_month_start
 )
-SELECT
-    Income.custom_month_start,
-    Income.Income,
-    Savings.Savings,
-    Roomate.Roomate,
-    Rent.Rent,
-    electricity_and_gas.Electricity_and_Gas,
-    water.Water,
-    internet.Internet,
-    municipality_tax.Municipality_Tax,
-    home_stuff.Home_Stuff,
-    coffee.Coffee,
-    insurance.Insurance,
-    phone.Phone,
-    car.Car,
-    monthly_spending.Monthly_Spending,
-    clothes.Clothes,
-    new_stuff.New_Stuff,
-    other.Other
 
-FROM Income
-LEFT JOIN Savings ON TRUE
-LEFT JOIN Roomate ON TRUE
-LEFT JOIN Rent ON TRUE
-LEFT JOIN electricity_and_gas ON TRUE
-LEFT JOIN water ON TRUE
-LEFT JOIN internet ON TRUE
-LEFT JOIN municipality_tax ON TRUE
-LEFT JOIN home_stuff ON TRUE
-LEFT JOIN coffee ON TRUE
-LEFT JOIN insurance ON TRUE
-LEFT JOIN phone ON TRUE
-LEFT JOIN car ON TRUE
-LEFT JOIN monthly_spending ON TRUE
-LEFT JOIN clothes ON TRUE
-LEFT JOIN new_stuff ON TRUE
-LEFT JOIN other ON TRUE
+SELECT *
+FROM aggregated
 ORDER BY custom_month_start DESC
